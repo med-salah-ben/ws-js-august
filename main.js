bf20d91901f7b52ff4106d8b4a5dad946bb63bf9
med-salah-ben <med.salah.ben.amou@gmail.com>
2022-08-24 17:12:09 +0100

    ws-js

---
+++ main.js
@@ -0,0 +1,214 @@
+// console.log("hello world!!!")
+// alert('linked file')
+// console.error("this is error")
+// console.warn("this is warn");
+// console.log(prompt("age"))
+
+//VAR
+// var x = 10;
+// //constant
+// const y = 20;
+// //let
+// let z = 15;
+
+// console.log(x)
+
+//PRIMITIVE TYPES
+//string
+// let str = "gomycoders";
+// //Number
+// var age = 25;
+// const x = 1.7;
+// //Boolen
+// let isDone = true;
+// //und
+// let z ; 
+// //null
+// let y = null ;
+// //NAN
+// let a = NaN;
+// console.log("age :",age,"x :",x,"isDone :",isDone,"z :",z,"y :",y,"a :",a)
+// console.log("a :" , a)
+// console.log(`hello ${str} i have ${age} years old `)
+
+// ARRAYS - Store multiple values in a variable
+
+// let arr = [10,2,15,-1,100,-23,9999]
+// let arr = ["hi","hello","welcome","marhaba"]
+// let arr = ["how are you", 1 , true,5,"five",false,true]
+// console.table(arr)
+// console.log(arr)
+
+//OBJECTS LITERALS
+// const age =30
+// const student = {
+//     name:"foulen",
+//     lastName:"ben foulen",
+//     age:45,
+//     isPerson:true,
+//     favFood:["couscous","pizza","kaftaji"]
+// }
+// console.log(student)
+
+// //String con
+// console.log(`my name is ${student.name} i have ${student.age} years old` )
+// console.log("my name is "+student.name+ " my last name is " + student.lastName + " i'm a person (" + student.isPerson +")" )
+
+//String Meth
+
+// let str = "HeLLo World!!!"
+
+// console.log(str.length)
+
+// console.log(str.toLocaleLowerCase())
+// console.log(str.toUpperCase())
+
+//Array Methods
+// const colors = ['black', 'white', 'red', 'blue'];
+// // console.log(colors[2])
+// // colors.push("yellow")
+// // colors.unshift("orange")
+// // colors.shift()
+// // colors.pop()
+// console.log(Array.isArray(colors))
+
+// Arith
+
+// let a = 10 ;
+// let b = 20;
+// console.log(a+b)
+// console.log(10+20)
+// console.log(20-5)
+// console.log(5*4)
+// console.log(20/5)
+// console.log(10%3)
+// console.log(4**2)
+// console.log(++a)
+// console.log("b : ", --b)
+// console.log("first" * a)
+// console.log("3"*3)
+// console.log("5"+2)
+// console.log("10"/"2")
+
+//Obejct
+
+// const person = { 
+//     firstName: 'Adam',
+//     age: 30,
+//     hobbies: ['music', 'movies', 'sports'],
+//     address: {
+//     street: '66 Main st',
+//     city: 'wtvr',
+//     state: 'over there'
+//     }
+//     };
+
+// person.email = "adam@gamil.com"
+// // console.log(person)
+
+// person.hobbies.push("camping")
+// console.log(person)
+
+//Array of Object
+// const student =[
+//     {
+//         name:"med",
+//         age:62
+//     },
+//     {
+//         name:"imen",
+//         age:20
+//     },
+//     {
+//         name:"firas",
+//         age:17
+//     }
+// ]
+
+// console.log(student)
+
+//compa
+
+var a = 5 
+var b = "5"
+
+// a = b
+// console.log(a)
+
+// console.log(a==b)
+// console.log(a===b)
+// console.log(a != b)
+// console.log(a !== b)
+
+// console.log(1 && 0 && 5 )
+// console.log(0 || "" || NaN || 1 || false)
+
+//Loops
+
+// for(i=0 ; i< 10  ; i++ ){
+//     console.log(`index is ${i}`)
+// }
+
+// let x =10;
+// while(x<15){
+//     console.log("x =" , x)
+//     x++
+// }
+// let y =15;
+
+// do {
+//     console.log("y", y)
+//     y++
+// } while (y < 0);
+
+// const numbers = [22, 55, 66];
+// for(i=0 ; i< 10  ; i++ ){
+//     console.log(numbers[i])
+// }
+
+
+// for(num in numbers){
+//     console.log(num)
+// }
+// for(num of numbers){
+//     console.log(num)
+// }
+
+// let x = 1 
+
+// if(x>=5){
+//     console.log("x greater or equal to 5")
+// }else{
+//     console.log("x less than 5")
+// }
+
+// x >= 5 ? console.log("x greater or equal to 5") : console.log("x less than 5")
+
+// switch (x) {
+//     case 1:
+//          console.log("x = 1")
+//          break
+//         case 2:
+//              console.log("x = 2")   
+//              break
+//             case 3:
+//                  console.log("x = 3")
+//                  break
+             
+
+//     default:
+//          console.log("x > 3");
+// }
+
+// function sum(a,b){
+//     return a+b
+// }
+
+// console.log(sum(80,9))
+
+// const add = (a,b)=>{
+//     return a+b
+// }
+// const min = (a)=> a-a
+// console.log(min(5))
+// console.log(add(81,9))
